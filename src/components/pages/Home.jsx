e2e6a2d91b61f37df32de39a17232894966070c5
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-03 13:36:32 +0000

    centres youtube videos

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -15,14 +15,8 @@ function Home() {
           allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-          style={{ marginTop: '30px' }}
+          style={{ marginTop: '30px', marginBottom: '-50px' }}
         ></iframe>
-      </div>
-      <div>
-        <img src={homeImage} alt="Caroline on the Beach" style={{ maxWidth: '100%' }} className="beach" />
-      </div>
-
-      <div>
         <iframe
           title="Spotify Player"
           src="https://open.spotify.com/embed/album/44wQo40Fk9xXBkNvSE3wE1?utm_source=generator"
@@ -31,9 +25,13 @@ function Home() {
           allowFullScreen=""
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"
-
         ></iframe>
       </div>
+
+      <div>
+        <img src={homeImage} alt="Caroline on the Beach" style={{ maxWidth: '100%' }} className="beach" />
+      </div>
+
     </div>
   );
 }
