975821b7413f9df9bff43c0137348fcfabc99755
CarolineBellaLane <carolinelane@live.co.uk>
2024-03-03 11:03:52 +0000

    changes tracks for single banners

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from "../../assets/images/logos-large/3.png";
 import logoThin from "../../assets/images/logo-thinner.png";
 import "./navbar.css";
 
 function CustomNavbar() {
+  const [menuExpanded, setMenuExpanded] = useState(false);
+
+  const handleLinkClick = () => {
+    setMenuExpanded(false); // Close the menu when a link is clicked
+  };
+
   return (
-    <Navbar expand="lg" className="bg-body-tertiary navbar">
+    <Navbar expand="lg" className="bg-body-tertiary navbar" expanded={menuExpanded}>
       <Container>
-        <Navbar.Brand as={Link} to="/">
+        <Navbar.Brand as={Link} to="/" onClick={() => setMenuExpanded(false)}>
           <img
             src={logoThin}
             alt="Caroline Bella Music Logo"
             style={{ maxHeight: '', maxWidth: '270px', borderRadius: 0, marginTop: '10px', marginBottom: '10px'}}
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setMenuExpanded(!menuExpanded)} />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto nav-links"> {/* Use mx-auto to spread out the links */}
+          <Nav className="mx-auto nav-links" onClick={handleLinkClick}>
             <Nav.Link as={Link} to="/" className="me-5">Home</Nav.Link>
             <Nav.Link as={Link} to="/gallery" className="me-5">Gallery</Nav.Link>
             <Nav.Link as={Link} to="/music" className="me-5">Music</Nav.Link>
@@ -29,8 +34,8 @@ function CustomNavbar() {
           </Nav>
           <Nav>
             <NavDropdown title="Contact" id="basic-nav-dropdown">
-              <NavDropdown.Item id="basic-nav-dropdown" href="mailto:cazbella@myyahoo.com">Email</NavDropdown.Item>
-              <NavDropdown.Item id="basic-nav-dropdown" href="https://www.facebook.com/CarolineBellaMusic" target="_blank">Facebook</NavDropdown.Item>
+              <NavDropdown.Item href="mailto:cazbella@myyahoo.com">Email</NavDropdown.Item>
+              <NavDropdown.Item href="https://www.facebook.com/CarolineBellaMusic" target="_blank">Facebook</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
--- src/components/pages/Music.jsx
+++ src/components/pages/Music.jsx
@@ -1,18 +1,55 @@
 import React from "react";
+import "../../components/pages/Music.css";
 
 function Music() {
   return (
-    <div style={{ height: "100%", overflowY: "scroll" , marginTop: '10px'}}>
-      {/* Embed SoundCloud Profile */}
-      <iframe className="player"
-        title="SoundCloud Profile"
-        width="100%"
-        height="1000"
-        scrolling="no"
-        frameborder="no"
-        allow="autoplay"
-        src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/carolinebellamusic&color=%234c4c4c&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
-      ></iframe>
+    <div>
+      {/* Embedded SoundCloud Tracks */}
+      <div className="musicTrack">
+        <iframe
+          className="trackBox"
+          width="100%"
+          height="100%"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://www.youtube.com/embed/J845TEcF0Uo?si=VkeG-8eiTGMARG0u"
+        ></iframe>
+      </div>
+      <div className="musicTrack">
+        <iframe
+          className="trackBox"
+          width="100%"
+          height="100%"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/86081576&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
+      <div className="musicTrack">
+        <iframe
+          className="trackBox"
+          width="100%"
+          height="100%"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/329355193&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        ></iframe>
+      </div>
+      <div className="musicTrack">
+        <iframe
+          className="trackBox"
+          width="100%"
+          height="100%"
+          scrolling="no"
+          frameBorder="no"
+          allow="autoplay"
+          src="https://www.youtube.com/embed/ZaaWuXPRU6A?si=qzrSLWRQGcM7Ej4o" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+
+      {/* Add more tracks as needed */}
     </div>
   );
 }
